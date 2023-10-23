765dd55335da644cebcd11d6b3e083fdfcd8e3c4
koo <koo@CEC-KOO-2N1>
2023-10-22 20:40:18 -0400

    Add project files.

---
+++ Source Code Management 1/Source.cpp
@@ -0,0 +1,13 @@
+// Joshua Koo
+// 10/21/23
+// This program connects Visual Studio code with GitHub
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello, World!" << endl;
+	return 0;
+
+}
